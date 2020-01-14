95b39dbc4f537c73eca2ab89e7c7c758c57b2d12
Martin Dahl <martin@netdev.se>
2020-01-14 14:05:33 +0100

    Added delete method to WalletResource

--- src/main/java/com/crankoid/cryptowalletservice/resource/wallet/api/WalletResource.java
+++ src/main/java/com/crankoid/cryptowalletservice/resource/wallet/api/WalletResource.java
@@ -1,13 +1,14 @@
 package com.crankoid.cryptowalletservice.resource.wallet.api;
 
+
 import com.crankoid.cryptowalletservice.resource.wallet.api.dto.WalletDTO;
 import com.crankoid.cryptowalletservice.resource.wallet.api.dto.UserId;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.math.BigInteger;
 
 @RequestMapping("/wallet")
 public interface WalletResource {
@@ -15,8 +16,11 @@ public interface WalletResource {
     @PostMapping
     String generateWallet(@RequestBody(required = true) UserId userId);
 
-    @GetMapping
-    WalletDTO getWallet(@RequestBody(required = true) UserId userId);
+    @GetMapping("/{userId}")
+    WalletDTO getWallet(@PathVariable(required = true) String userId);
+
+    @DeleteMapping("/{userId}")
+    boolean deleteWallet(@PathVariable(required = true) String userId);
 
     @PostMapping("/send")
     String sendBitcoinPayment(@RequestBody(required = true) String sourceUserId,
